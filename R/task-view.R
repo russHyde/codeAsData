37676285507d7d758c7b1bee97f18df086dbdebc
Russ Hyde <russ.hyde.data@gmail.com>
2023-03-17 14:36:08 +0000

    refac: depipe the task-view functions

--- R/task-view.R
+++ R/task-view.R
@@ -5,8 +5,8 @@
 #' @export
 
 import_task_view_packages <- function(url) {
-  xml2::read_xml(url) %>%
-    extract_package_names()
+  xml <- xml2::read_xml(url)
+  extract_package_names(xml)
 }
 
 #' Extract package names from the xml for a .ctv formatted file
@@ -18,7 +18,6 @@ extract_package_names <- function(xml) {
   # and the list of <pkg> tags is stored in a <packagelist> tag
 
   # extract the package names
-  xml %>%
-    xml2::xml_find_all("packagelist/pkg") %>%
-    xml2::xml_text()
+  package_nodes <- xml2::xml_find_all(xml, "packagelist/pkg")
+  xml2::xml_text(package_nodes)
 }
