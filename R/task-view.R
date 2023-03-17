22ae20c803305f8b083c3d3c907855e6cf47ec60
Russ Hyde <russ.hyde.data@gmail.com>
2023-03-17 15:52:40 +0000

    feat: import_task_view_packages can read task-view markdown files

--- R/task-view.R
+++ R/task-view.R
@@ -1,10 +1,30 @@
 #' Import a vector of package names from a "Task View" page
 #'
-#' @param   url   A URL containing a Task View page.
+#' The file can either be a legacy `.ctv` file or a modern markdown-format task-view.
+#'
+#' @param   url   A filepath or URL containing a Task View page
+#' @return   Character vector of package names
 #'
 #' @export
 
 import_task_view_packages <- function(url) {
+  if (tools::file_ext(url) == "ctv") {
+    packages <- import_task_view_packages_ctv(url)
+    return(packages)
+  }
+
+  task_view <- ctv::read.ctv(url)
+  task_view[["packagelist"]][["name"]]
+}
+
+#' Import a vector of package names from a (legacy) xml-format "Task View" page
+#'
+#' The old format of task-views used a ".ctv" file format, that was xml based.
+#'
+#' @param   url   A filepath or URL pointing to a .ctv file
+#' @return   Character vector of package names
+
+import_task_view_packages_ctv <- function(url) {
   xml <- xml2::read_xml(url)
   extract_package_names(xml)
 }
